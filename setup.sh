4cc448423f3386e33be8dfdead56980484dea789
Sujaykumar. Hublikar <hello@sujaykumarh.com>
2022-01-12 20:59:42 +0530

    🎨 (setup): add setup skeleton

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#####
+## dotfiles setup script
+#####
+
+# ########################################################################
+# Copyright (C) 2022 Sujaykumar. Hublikar <hello@sujaykumarh.com>        #
+# Licensed under the MIT License.                                        #
+# ########################################################################
---
+++ unattended.sh
@@ -0,0 +1,8 @@
+#####
+## dotfiles setup script - unattended
+#####
+
+# ########################################################################
+# Copyright (C) 2022 Sujaykumar. Hublikar <hello@sujaykumarh.com>        #
+# Licensed under the MIT License.                                        #
+# ########################################################################
