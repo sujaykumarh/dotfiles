cb2bfdc29ee443edb2154c51ec1fc5b370a6b682
Sujaykumar. Hublikar <hello@sujaykumarh.com>
2022-05-20 08:59:03 +0530

    🐛 (aliases): update aliases

--- aliases/whatsmyip.sh
+++ aliases/whatsmyip.sh
@@ -1,10 +1,11 @@
 ## Find out System Hostname, External IP and internal ip using python script
-_whatsMyIP(){
-    if [[ `which python` == "python not found" ]]; then
-        echo "Python not found! Please install python"
-        return
-    fi
 
+if [[ `which python` == "python not found" ]]; then
+    # echo "Python not found! Please install python"
+    return
+fi
+
+_whatsMyIP(){
     python $DOTFILES_DIR/scripts/whatsmyip.py "$@"
 }
 alias whatsmyip=_whatsMyIP
